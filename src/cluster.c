4f9deafa90afb3d247efc08a6d0375e077cfbd6e
Idoia Ochoa <idoia.ochoa@gmail.com>
2017-11-01 14:21:55 -0500

    master version first commit
    Current version with new AUX compression and stream mode

---
+++ src/cluster.c
@@ -0,0 +1,221 @@
+/**
+ * k-means clustering implementation in C
+ * 
+ * The approach here is parallelizable and should be migrated to a block-based implementation
+ * using opencl in order to run faster, or possibly just multithreaded, but I have left it
+ * in plain C for the time being to get it working quickly. Note that the means established
+ * are discrete values, rather than continuous.
+ */
+
+#include "util.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+//#include <malloc.h>
+
+#include "pmf.h"
+#include "codebook.h"
+#include "cluster.h"
+
+/**
+ * Allocate the memory used for the clusters based on the number wanted and column config
+ */
+struct cluster_list_t *alloc_cluster_list(struct quality_file_t *info) {
+	uint8_t j;
+	struct cluster_list_t *rtn = (struct cluster_list_t *) calloc(1, sizeof(struct cluster_list_t));
+
+	// Allocate array of cluster structures
+	rtn->count = info->cluster_count;
+	rtn->clusters = (struct cluster_t *) calloc(info->cluster_count, sizeof(struct cluster_t));
+
+	// Fill in each cluster
+	for (j = 0; j < info->cluster_count; ++j) {
+		rtn->clusters[j].id = j;
+		rtn->clusters[j].count = 0;
+		rtn->clusters[j].mean.data = (symbol_t *) calloc(info->columns, sizeof(symbol_t));
+		// mean.cluster is skipped because it is unused
+		// mean.distances remains a null pointer because it is unused
+		rtn->clusters[j].members = (struct line_t **) calloc(info->lines, sizeof(struct line_t *));
+		rtn->clusters[j].training_stats = alloc_conditional_pmf_list(info->alphabet, info->columns);
+	}
+
+	return rtn;
+}
+
+/**
+ * Deallocate the memory used for the clusters.
+ */
+void free_cluster_list(struct cluster_list_t *clusters) {
+	uint8_t j;
+
+	for (j = 0; j < clusters->count; ++j) {
+		free(clusters->clusters[j].mean.data);
+		free(clusters->clusters[j].members);
+		free_conditional_pmf_list(clusters->clusters[j].training_stats);
+	}
+	free(clusters->clusters);
+	free(clusters);
+}
+
+/**
+ * Calculates cluster assignments for the block of lines given, and return
+ * status indicating that at least one line changed clusters
+ */
+uint8_t cluster_lines(struct line_block_t *block, struct quality_file_t *info) {
+	uint32_t i;
+	uint8_t changed = 0;
+
+	for (i = 0; i < block->count; ++i) {
+		changed |= do_cluster_assignment(&block->lines[i], info);
+	}
+
+	return changed;
+}
+
+/**
+ * Updates the cluster means based on their assigned lines. Also clears the line count for
+ * the next iteration.
+ */
+void recalculate_means(struct quality_file_t *info) {
+	uint8_t i;
+
+	for (i = 0; i < info->cluster_count; ++i) {
+		calculate_cluster_mean(&info->clusters->clusters[i], info);
+	}
+}
+
+/**
+ * Calculates the mean for a cluster.
+ */
+void calculate_cluster_mean(struct cluster_t *cluster, struct quality_file_t *info) {
+	struct line_t *line;
+	uint32_t i, j;
+	uint64_t *accumulator = (uint64_t *) _alloca(info->columns*sizeof(uint64_t));
+
+	// Clear previous
+	memset(accumulator, 0, info->columns*sizeof(uint64_t));
+
+	// Sum up everything column-wise
+	for (i = 0; i < cluster->count; ++i) {
+		line = cluster->members[i];
+		for (j = 0; j < info->columns; ++j) {
+			accumulator[j] += line->data[j];
+		}
+	}
+
+	// Integer division to find the mean, guaranteed to be less than the alphabet size
+	for (j = 0; j < info->columns; ++j) {
+		cluster->mean.data[j] = (uint8_t) (accumulator[j] / cluster->count);
+	}
+}
+
+/**
+ * Compare each line to each cluster to find distances
+ */
+uint8_t do_cluster_assignment(struct line_t *line, struct quality_file_t *info) {
+	uint8_t i;
+
+	for (i = 0; i < info->cluster_count; ++i) {
+		find_distance(line, &info->clusters->clusters[i], info);
+	}
+
+	return assign_cluster(line, info);
+}
+
+/**
+ * Assigns a cluster based on the one with the lowest distance
+ */
+uint8_t assign_cluster(struct line_t *line, struct quality_file_t *info) {
+	uint8_t id = 0;
+	uint8_t prev_id = line->cluster;
+	uint8_t i;
+	struct cluster_t *cluster;
+	double d = line->distances[0];
+
+	// Find the cluster with minimum distance
+	for (i = 1; i < info->cluster_count; ++i) {
+		if (line->distances[i] < d) {
+			id = i;
+			d = line->distances[i];
+		}
+	}
+
+	// Assign to that cluster
+	line->cluster = id;
+	cluster = &info->clusters->clusters[id];
+	cluster->members[cluster->count] = line;
+	cluster->count += 1;
+
+	return (prev_id == id) ? 0 : 1;
+}
+
+/**
+ * Take a line and cluster information and calculates the distance, storing it in the line information vector
+ */
+void find_distance(struct line_t *line, struct cluster_t *cluster, struct quality_file_t *info) {
+	double d = 0.0;
+	uint32_t i;
+	uint32_t data, mean;
+
+	for (i = 0; i < info->columns; ++i) {
+		data = line->data[i];
+		mean = cluster->mean.data[i];
+		//d += (data - mean) * (data - mean);
+		d += get_distortion(info->dist, data, mean);
+	}
+	line->distances[cluster->id] = d;
+}
+
+/**
+ * Initialize the cluster means based on the data given, using random selection
+ */
+void initialize_kmeans_clustering(struct quality_file_t *info) {
+	uint8_t j;
+	uint32_t block_id;
+	uint32_t line_id;
+	struct cluster_list_t *clusters = info->clusters;
+
+	for (j = 0; j < info->cluster_count; ++j) {
+		block_id = rand() % info->block_count;
+		line_id = rand() % info->blocks[block_id].count;
+		memcpy(clusters->clusters[j].mean.data, info->blocks[block_id].lines[line_id].data, info->columns*sizeof(uint8_t));
+		if (info->opts->verbose) {
+			printf("Chose block %d, line %d.\n", block_id, line_id);
+		}
+	}
+}
+
+/**
+ * Do k-means clustering over the set of blocks given to produce a set of clusters that
+ * fills the cluster list given
+ */
+void do_kmeans_clustering(struct quality_file_t *info) {
+	uint32_t iter_count = 0;
+	uint8_t changed = 1;
+	uint32_t j;
+	struct cluster_list_t *clusters = info->clusters;
+
+	initialize_kmeans_clustering(info);
+
+	while (iter_count < MAX_KMEANS_ITERATIONS && changed) {
+		for (j = 0; j < clusters->count; ++j) {
+			clusters->clusters[j].count = 0;
+		}
+
+		changed = 0;
+		for (j = 0; j < info->block_count; ++j) {
+			changed |= cluster_lines(&info->blocks[j], info);
+		}
+
+		recalculate_means(info);
+		iter_count += 1;
+		if (info->opts->verbose) {
+			printf(".");
+		}
+	}
+
+	if (info->opts->verbose) {
+		printf("\nTotal number of iterations: %d.\n", iter_count);
+	}
+}
---
+++ src/lines.c
@@ -0,0 +1,152 @@
+/**
+ * Utility functions for manipulating the data from files, like reading it into memory
+ * and converting between the different formats we use
+ */
+
+#include "util.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "lines.h"
+
+/**
+ * This reads data from the given file pointer into memory, breaking it into segments
+ * of the given number of lines, to ease memory management issues at the cost of some
+ * overhead. This assumes that the file consists entirely of quality scores with no
+ * other lines in between
+ * @param path Path of the file to read
+ * @param info Information structure to store in, this must be a valid pointer already
+ * @param max_lines Maximum number of lines to read, will override the actual number in the file if >0
+ */
+uint32_t load_file(const char *path, struct quality_file_t *info, uint64_t max_lines) {
+	uint32_t status, block_idx, line_idx;
+	uint32_t j;
+	char line[READ_LINEBUF_LENGTH];
+	FILE *fp;
+	struct _stat finfo;
+
+	// Load metadata into the info structure
+	info->path = strdup(path);
+	fp = fopen(path, "rt");
+	if (!fp) {
+		return LF_ERROR_NOT_FOUND;
+	}
+
+	// Use the first line to figure out how long the file is
+	fgets(line, READ_LINEBUF_LENGTH, fp);
+	info->columns = strlen(line) - 1;
+	if (info->columns > MAX_READS_PER_LINE) {
+		fclose(fp);
+		return LF_ERROR_TOO_LONG;
+	}
+
+	// Figure out how many lines we'll need depending on whether we were limited or not
+	_stat(path, &finfo);
+	info->lines = finfo.st_size / ((uint64_t) (info->columns+1));
+	if (max_lines > 0 && info->lines > max_lines) {
+		info->lines = max_lines;
+	}
+	
+	status = alloc_blocks(info);
+	if (status != LF_ERROR_NONE)
+		return status;
+
+	// Process the file
+	block_idx = 0;
+	line_idx = 0;
+	fseek(fp, 0, SEEK_SET);
+	while (!feof(fp) && (block_idx * MAX_LINES_PER_BLOCK + line_idx) < info->lines) {
+		// Read line and store in our array with data conversion, also stripping newlines
+		fgets(line, READ_LINEBUF_LENGTH, fp);
+		for (j = 0; j < info->columns; ++j) {
+			info->blocks[block_idx].lines[line_idx].data[j] = line[j] - 33;
+		}
+
+		// Increment line/block pointers as necesary
+		line_idx += 1;
+		if (line_idx == info->blocks[block_idx].count) {
+			line_idx = 0;
+			block_idx += 1;
+		}
+	}
+
+	fclose(fp);
+	return LF_ERROR_NONE;
+}
+
+/**
+ * Allocate an array of line block pointers and the memory within each block, so that we can
+ * use it to store the results of reading the file
+ */
+uint32_t alloc_blocks(struct quality_file_t *info) {
+	uint64_t lines_left = info->lines;
+	symbol_t *sym_buf;
+	double *dist_buf;
+	struct line_block_t *cblock;
+	struct line_t *cline;
+	uint32_t i;
+
+	// Figure out how many blocks we'll need to store this file
+	info->block_count = (uint32_t) (info->lines / (uint64_t)MAX_LINES_PER_BLOCK);
+	if (info->block_count * MAX_LINES_PER_BLOCK != info->lines) {
+		info->block_count += 1;
+	}
+
+	info->blocks = (struct line_block_t *) calloc(info->block_count, sizeof(struct line_block_t));
+	if (!info->blocks) {
+		return LF_ERROR_NO_MEMORY;
+	}
+	cblock = info->blocks;
+
+	while (lines_left > 0) {
+		// Figure out how many lines we'll have in this block
+		if (lines_left > MAX_LINES_PER_BLOCK) {
+			lines_left -= MAX_LINES_PER_BLOCK;
+			cblock->count = MAX_LINES_PER_BLOCK;
+		}
+		else {
+			cblock->count = (uint32_t) lines_left;
+			lines_left = 0;
+		}
+
+		// Allocate symbol buffer and array of line info structs for the block
+		sym_buf = (symbol_t *) calloc(cblock->count, info->columns*sizeof(symbol_t));
+		dist_buf = (double *) calloc(cblock->count, info->cluster_count*sizeof(double));
+		cblock->lines = (struct line_t *) calloc(cblock->count, sizeof(struct line_t));
+		cline = cblock->lines;
+		if (!sym_buf || !cblock->lines || !dist_buf) {
+			return LF_ERROR_NO_MEMORY;
+		}
+
+		// Save pointers into the symbol buffer for each line in the block
+		for (i = 0; i < cblock->count; ++i) {
+			cline->data = sym_buf;
+			cline->distances = dist_buf;
+			cline += 1;
+			sym_buf += info->columns;
+			dist_buf += info->cluster_count;
+		}
+
+		// Advance to the next line block
+		cblock += 1;
+	}
+
+	return LF_ERROR_NONE;
+}
+
+/**
+ * Deallocates the memory used to store file information in blocks
+ */
+void free_blocks(struct quality_file_t *info) {
+	// Array of block pointers is a single allocation
+	// For each block, array of lines and array of symbols are both single allocations
+	uint32_t i;
+
+	for (i = 0; i < info->block_count; ++i) {
+		free(info->blocks[i].lines[0].data);
+		free(info->blocks[i].lines[0].distances);
+		free(info->blocks[i].lines);
+	}
+	free(info->blocks);
+}
